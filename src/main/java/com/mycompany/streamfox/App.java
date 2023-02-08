9f548c438b3116b0e9d75620f59293720bd1c7c0
homepc <punishedtomato@gmail.com>
2023-02-07 19:41:33 -0500

    removed default fxml and controller, added gitignore which includes IntelliJ idea configuration files and compilation target files, removed default method from PrimaryController, added exit method to PrimaryController, added fullscreen method to PrimaryController, added primarystylesheet.css to resources folder, added fullscreen method to App, adjusted interface to have rounded corners with dark gray background, adjusted interface to have two buttons for exiting the application and toggling fullscreen, added javadoc comments to new methods.

--- src/main/java/com/mycompany/streamfox/App.java
+++ src/main/java/com/mycompany/streamfox/App.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,16 +16,35 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Stage stage;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        //variables
+        int width = 640;
+        int height = 480;
+        App.stage = stage;
+
+        //stylesheet
+        String css = this.getClass().getResource("primarystylesheet.css").toExternalForm();
+
+        //scene
+        scene = new Scene(loadFXML("primary"), width, height);
+        scene.getStylesheets().add(css);
+        scene.setFill(Color.TRANSPARENT);
+
+        //stage
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setFullScreenExitHint("");
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    /**
+     * changes the application to be fullscreen if it is not fullscreen, or exits fullscreen if it is
+     */
+    public static void fullscreen(){
+        stage.setFullScreen(!stage.isFullScreen());
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
--- src/main/java/com/mycompany/streamfox/PrimaryController.java
+++ src/main/java/com/mycompany/streamfox/PrimaryController.java
@@ -1,12 +1,27 @@
 package com.mycompany.streamfox;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class PrimaryController {
 
+    public VBox PrimaryTopPane;
+
+    /**
+     * exits the application
+     */
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void exit(){
+        System.out.println("exit");
+        System.exit(0);
+    }
+
+    /**
+     * switches the application to and from fullscreen mode
+     */
+    @FXML
+    private void fullscreen(){
+        System.out.println("fullscreen");
+        App.fullscreen();
     }
 }

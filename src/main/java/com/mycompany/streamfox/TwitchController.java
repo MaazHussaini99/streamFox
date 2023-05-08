d0ea426eae40bff6742fd122272dc5de19ced43a
Dnemov2000 <49795631+Dnemov2000@users.noreply.github.com>
2023-05-07 20:57:24 -0400

    chat
    added mini chat to twitch

---
+++ src/main/java/com/mycompany/streamfox/TwitchController.java
@@ -0,0 +1,373 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.streamfox;
+
+import com.google.api.services.youtube.model.CommentThread;
+import com.google.api.services.youtube.model.CommentThreadListResponse;
+import com.google.api.services.youtube.model.SearchListResponse;
+import static com.mycompany.streamfox.PrimaryVideoController.channelStartText;
+import static com.mycompany.streamfox.PrimaryVideoController.startVid;
+import static com.mycompany.streamfox.PrimaryVideoController.titleStartText;
+import com.teamdev.jxbrowser.browser.Browser;
+import com.teamdev.jxbrowser.engine.Engine;
+import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.engine.ProprietaryFeature;
+import static com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED;
+import com.teamdev.jxbrowser.view.javafx.BrowserView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import static javafx.collections.FXCollections.observableArrayList;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.util.Duration;
+import com.teamdev.jxbrowser.fullscreen.event.FullScreenEvent;
+import com.teamdev.jxbrowser.fullscreen.*;
+import javafx.stage.Screen;
+
+public class TwitchController implements Initializable {
+
+    @FXML
+    private AnchorPane backPane;
+
+    @FXML
+    private ImageView channelPic;
+
+    @FXML
+    private Label channelTxt;
+
+    @FXML
+    private ImageView closeWindow;
+
+    @FXML
+    private VBox videoView;
+
+    @FXML
+    private ImageView closeWindow1;
+
+    @FXML
+    private AnchorPane frontPane;
+
+    @FXML
+    private ImageView menuOpen;
+
+    @FXML
+    private ImageView minimizeWindow;
+
+    @FXML
+    private VBox recommendedTab;
+
+    @FXML
+    private TextField searchTxtField;
+
+    @FXML
+    private Label titleTxt;
+
+    @FXML
+    private AnchorPane topBar;
+
+    @FXML
+    private Button userNameMenuBtn;
+
+    @FXML
+    private Circle userProfView;
+
+    @FXML
+    private AnchorPane basePane;
+
+    @FXML
+    private VBox browserVBox;
+
+    @FXML
+    private VBox webVBox;
+
+    @FXML
+    private AnchorPane aP;
+
+    private WebEngine we;
+
+    private int onOff = 0;
+
+    public static VidObj[] Searchresults;
+
+    VBox[] testvb;
+
+    User user = User.getInstance();
+    UserData userData = UserData.getInstance();
+
+    SearchListResponse relatedVids;
+    CommentThreadListResponse comments;
+
+    static String startVid;
+    static String titleStartText;
+    static String channelStartText;
+
+    private EngineOptions options;
+    private Engine engine;
+    private Browser browser;
+    private BrowserView view;
+       @FXML
+    private WebView chatroom;
+
+    WebEngine chatroomengine;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        //  System.out.println("Hello");
+        chatroomengine = chatroom.getEngine();
+        if (App.stage.isFullScreen() == false) {
+              
+                chatroom.setMinSize(200, 100);
+        }
+        chatroomengine.loadContent("<!DOCTYPE html>\n"
+                + "<html lang=\"en\">\n"
+                + "	<head>\n"
+                + "		<meta charset=\"UTF-8\" />\n"
+                + "		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n"
+                + "		<meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />\n"
+                + "		<link\n"
+                + "			rel=\"stylesheet\"\n"
+                + "			href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css\"\n"
+                + "			integrity=\"sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=\"\n"
+                + "			crossorigin=\"anonymous\"\n"
+                + "		/>\n"
+                + "		<link rel=\"stylesheet\" href=\"css/style.css\" />\n"
+                + "    \n"
+                + "		<title>StreamFox </title>\n"
+                + "	</head>\n"
+                + "	<body>\n"
+                + "	<Font Color = \"White\" >\n"
+                + "	<body style=\"background-color: 	 black	;\">\n"
+                + "		<div class=\"join-container\">\n"
+                + "			<header class=\"join-header\">\n"
+                + "				<h1><i class=\"fa fa-comments\" ;\" ></i> StreamFox Chat, <Font Color = \"White\">  </h1>\n"
+                + "			</header>\n"
+                + "			<main class=\"join-main\">\n"
+                + "				<form action=\"chat.html\">\n"
+                + "					<div class=\"form-control\">\n"
+                + "						<label for=\"username\">Username</label>\n"
+                + "						<input\n"
+                + "							type=\"text\"\n"
+                + "							name=\"username\"\n"
+                + "							id=\"username\"\n"
+                + "							placeholder=\"Streamfox username...\"\n"
+                + "							required\n"
+                + "						/>\n"
+                + "					</div>\n"
+                + "					<div class=\"form-control\">\n"
+                + "                        <p>Enter the name of your ChatRoom:</p>\n"
+                + "<input \n"
+                + "							type=\"text\"\n"
+                + "							name=\"key\"\n"
+                + "							placeholder=\"chatroom ...\"\n"
+                + "							required\n"
+                + "id=\"key\" onkeyup='updateRedirect()' class='center'>\n"
+                + "<br>\n"
+                + "<br>\n"
+                + "<a class=\"btn\" id='join' href='javascript:;' type=\"submit\">Join</a>\n"
+                + "<br>\n"
+                + "<br>\n"
+                + "\n"
+                + "					</div>\n"
+                + "				</form>\n"
+                + "			</main>\n"
+                + "		</div>\n"
+                + "        <script>\n"
+                + "        function updateRedirect() {\n"
+                + "document.getElementById(\"join\").href = \"https://d66f3864-46f8-47cf-a043-50b7ef7731af.id.repl.co//chat.html?username=\"+document.getElementById(\"username\").value+\"&room=\"+document.getElementById(\"key\").value;\n"
+                + "style=\"background-color: Gray	;\"\n"
+                + "\n"
+                + "\n"
+                + "}\n"
+                + "        </script>\n"
+                + "\n"
+                + "	</body>\n"
+                + "\n"
+                + "</html>");
+
+       
+        
+        options = EngineOptions.newBuilder(HARDWARE_ACCELERATED)
+                .enableProprietaryFeature(ProprietaryFeature.AAC)
+                .enableProprietaryFeature(ProprietaryFeature.H_264)
+                .enableProprietaryFeature(ProprietaryFeature.WIDEVINE)
+                .licenseKey("1BNDHFSC1G5ZOFBWG6WQUSLCBTDAYZZXMAP2GRH6RECP8NHENP4ZY4YHBV1MUUDQTXFCFF")
+                .build();
+
+        engine = Engine.newInstance(options);
+        browser = engine.newBrowser();
+        //loadPage(startVid);
+        browser.navigation().loadUrl("https://player.twitch.tv/?channel="+channelStartText+"&parent=localhost&autoplay=false");
+        view = BrowserView.newInstance(browser);
+        view.setPrefSize(512, 288);
+
+        view.setVisible(true);
+
+//        FullScreen fullScreen = browser.fullScreen();
+//        fullScreen.addFullScreenListener(event -> {
+//            if (event.isFullScreen()) {
+//                System.out.println("Entered full screen mode");
+//            } else {
+//                System.out.println("Exited full screen mode");
+//            }
+//        });
+        videoView.getChildren().add(view);
+
+        frontPane.setVisible(false);
+        FadeTransition ft = new FadeTransition(Duration.seconds(0.5), frontPane);
+        ft.setFromValue(1);
+        ft.setToValue(0);
+        ft.play();
+
+        TranslateTransition tt = new TranslateTransition(Duration.seconds(0.1), frontPane);
+        tt.setByX(-200);
+        tt.play();
+
+        topBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                App.xOffset = event.getSceneX();
+                App.yOffset = event.getSceneY();
+            }
+        });
+
+        //move around here
+        topBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                App.stage.setX(event.getScreenX() - App.xOffset);
+                App.stage.setY(event.getScreenY() - App.yOffset);
+            }
+        });
+
+
+        titleTxt.setText(titleStartText);
+        channelTxt.setText(channelStartText);
+        System.out.println(startVid);
+
+        userNameMenuBtn.setText(((String) userData.getProfileDataMap().get("fname")) + " " + ((String) userData.getProfileDataMap().get("lname")));
+        userProfView.setFill(new ImagePattern(new Image((String) userData.getProfileDataMap().get("profileImage"))));
+
+    }
+
+    @FXML
+    void searchFunction(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void closeCommand(MouseEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void switchToHome(ActionEvent event) throws IOException {
+        App.setRoot("primary_Home");
+    }
+
+    @FXML
+    void switchToYT(ActionEvent event) throws IOException {
+        App.setRoot("Youtube");
+    }
+
+    @FXML
+    void switchToTwitch(ActionEvent event) throws IOException {
+        App.setRoot("Twitch_video_");
+    }
+
+    @FXML
+    void switchToProfile(ActionEvent event) throws IOException {
+        App.setRoot("primary_Profile");
+    }
+
+    @FXML
+    void switchToSettings(ActionEvent event) throws IOException {
+        App.setRoot("Settings");
+    }
+
+    @FXML
+    void minimizeCommand(MouseEvent event) {
+        App.stage.setIconified(true);
+    }
+
+    /**
+     * exits the application
+     */
+    @FXML
+    private void exit() {
+        System.out.println("exit");
+        System.exit(0);
+    }
+
+    /**
+     * switches the application to and from fullscreen mode
+     */
+    @FXML
+    private void fullscreen() {
+        App.fullscreen();
+        if (App.stage.isFullScreen() == false) {
+            view.setPrefSize(512, 288);
+        } else {
+            view.setPrefSize(1024, 576);
+        }
+    }
+
+    @FXML
+    void menuMove(MouseEvent event) {
+        if (onOff == 0) {
+            frontPane.setVisible(true);
+            FadeTransition ft = new FadeTransition(Duration.seconds(0.2), frontPane);
+            ft.setFromValue(0);
+            ft.setToValue(1);
+            ft.play();
+
+            TranslateTransition tt = new TranslateTransition(Duration.seconds(0.2), frontPane);
+            tt.setByX(+200);
+            tt.play();
+
+            onOff = 1;
+        } else if (onOff == 1) {
+
+            FadeTransition ft = new FadeTransition(Duration.seconds(0.1), frontPane);
+            ft.setFromValue(1);
+            ft.setToValue(0);
+            ft.play();
+
+            TranslateTransition tt = new TranslateTransition(Duration.seconds(0.2), frontPane);
+            tt.setByX(-200);
+            tt.play();
+            //frontPane.setVisible(false);
+
+            onOff = 0;
+        }
+
+    }
+
+    public void loadPage(String vid) {
+        //browser.navigation().loadUrl("https://player.twitch.tv/?channel=shroud&parent=localhost&autoplay=false");
+        //we.load("https://player.twitch.tv/?video=41280532217&parent=localhost&autoplay=false");
+    }
+}
---
+++ src/main/java/com/mycompany/streamfox/TwitchVids.java
@@ -0,0 +1,177 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.streamfox;
+
+import com.google.api.client.util.Data;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+import javafx.fxml.FXML;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+/**
+ *
+ * @author maazh
+ */
+public class TwitchVids {
+
+
+    
+    
+    private static TwitchApiEngine tae = new TwitchApiEngine();
+
+    public static String gameRequest() throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .callTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://api.twitch.tv/helix/games/top")
+                .get()
+                .addHeader("Client-ID", "l8en5dbypf4crtypbx0n8k9vbcxygw")
+                .addHeader("Authorization", "Bearer " + tae.accessToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println("Status: " + response.code());
+            //System.out.println(response.body().string());
+            String responseBody = response.body().string();
+            return responseBody;
+        }
+    }
+
+    public static VidObj[][] getTopVideos() throws IOException {
+        String id;
+        String boxArt;
+        VidObj[][] array = new VidObj[5][];
+        String games = gameRequest();
+
+        JSONObject json = new JSONObject(games);
+
+        JSONArray dataArray = json.getJSONArray("data");
+
+        for (int j = 0; j < 5; j++) {
+            JSONObject dataObj = dataArray.getJSONObject(j);
+            System.out.println(dataObj);
+            id = dataObj.getString("id");
+            boxArt = dataObj.getString("box_art_url");
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .callTimeout(30, TimeUnit.SECONDS)
+                    .build();
+
+            HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.twitch.tv/helix/streams").newBuilder();
+            urlBuilder.addQueryParameter("game_id", id);
+            urlBuilder.addQueryParameter("first", "50");
+            String url = urlBuilder.build().toString();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .get()
+                    .addHeader("Client-ID", "l8en5dbypf4crtypbx0n8k9vbcxygw")
+                    .addHeader("Authorization", "Bearer " + tae.accessToken)
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                System.out.println("Top Videos - Status: " + response.code());
+                //System.out.println(response.body().string());
+
+                String responseBody = response.body().string();
+                System.out.println(responseBody);
+                JSONObject json1 = new JSONObject(responseBody);
+                VidObj[] arr = new VidObj[50];
+                JSONArray dataArray1 = json1.getJSONArray("data");
+                for (int i = 0; i < dataArray1.length(); i++) {
+
+                    JSONObject dataObj1 = dataArray1.getJSONObject(i);
+
+                    String id1 = dataObj1.getString("id");
+                    String title = dataObj1.getString("title");
+                    String channel = dataObj1.getString("user_name");
+                    String thumbnail = dataObj1.getString("thumbnail_url");
+//                  System.out.println("Title: " + title);
+                    VidObj vid = new VidObj(id1, title, channel, id, thumbnail, boxArt);
+                    arr[i] = vid;
+                }
+                array[j] = arr;
+            }
+            //System.out.println("***************************");
+            //System.out.println(arr.toString());
+        }
+        System.out.println(array[0][0].title);
+        System.out.println(array[2][0].title);
+        return array;
+    }
+
+    public static void getVideo(String id) throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .callTimeout(30, TimeUnit.SECONDS)
+                .build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.twitch.tv/helix/videos").newBuilder();
+
+        urlBuilder.addQueryParameter("id", id);
+
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Client-ID", "l8en5dbypf4crtypbx0n8k9vbcxygw")
+                .addHeader("Authorization", "Bearer " + tae.accessToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println("Top Videos - Status: " + response.code());
+            String responseBody = response.body().string();
+
+            JSONObject json = new JSONObject(responseBody);
+
+            JSONArray dataArray = json.getJSONArray("data");
+            for (int i = 0; i < dataArray.length(); i++) {
+                JSONObject dataObj = dataArray.getJSONObject(i);
+                String title = dataObj.getString("title");
+                System.out.println(title);
+            }
+        }
+    }
+
+    public static void getStreams() throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .callTimeout(30, TimeUnit.SECONDS)
+                .build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.twitch.tv/helix/streams").newBuilder();
+
+        urlBuilder.addQueryParameter("game_id", "32399");
+
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Client-ID", "l8en5dbypf4crtypbx0n8k9vbcxygw")
+                .addHeader("Authorization", "Bearer " + tae.accessToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println("Top Videos - Status: " + response.code());
+            System.out.println(response.body().string());
+        }
+    }
+
+}
